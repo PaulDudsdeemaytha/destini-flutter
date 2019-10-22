849fbb1a855c0e98ee1a5cebfdabf456e57a511a
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-10-22 09:59:20 -0700

    Created methods that returned the initial text and choices for the story

--- lib/story.dart
+++ lib/story.dart
@@ -1,10 +1,9 @@
 //TODO: Step 2 - Create a new class called Story.
 class Story {
-  String storyText;
+  String storyTitle;
   String choice1;
   String choice2;
-
-  Story(this.storyText, this.choice1, this.choice2);
+  Story({this.storyTitle, this.choice1, this.choice2});
 }
 //TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
 
